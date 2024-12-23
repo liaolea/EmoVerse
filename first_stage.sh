f1aaa0f8cc978325eadfb867d235df60fdf84acb
XinYiLea <103017550+xiaoyaoxinyi@users.noreply.github.com>
2024-12-23 15:52:21 +0800

    Add files via upload

---
+++ first_stage.sh
@@ -0,0 +1,18 @@
+CUDA_VISIBLE_DEVICES=1 \
+swift sft \
+  --model_type internvl2-4b \
+  --model_id_or_path InternVL2-4B_model_path \
+  --dataset emt_data_first.json \
+  --sft_type lora \
+  --output_dir output \
+  --system "You are an sentiment and emotion analysis expert." \
+  --num_train_epochs 2 \
+  --max_length 2048 \
+  --gradient_checkpointing true \
+  --batch_size 1 \
+  --gradient_accumulation_steps 8 \
+  --eval_steps 1000 \
+  --save_steps 500 \
+  --save_total_limit 3 \
+  --logging_steps 10 \
+  --acc_strategy sentence
